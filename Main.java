5b6201f98245fa998f0010c004e1e44eeb175121
Dani Hassanieh <dioritoni@outlook.com>
2023-03-03 16:46:40 +0000

    added getSum, copyOfRange, toString, edited javadoc wording, added javadoc placeholders

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ import automathicalc.*;
 class Main {
   public static void main(String[] args)
   {
+    double[] ryamu = new double[]{2, 4, 6};
+    System.out.println(Statistics.toString(ryamu));
+    System.out.println(ryamu);
+    System.out.println(Statistics.getSum(ryamu));
+    System.out.println(Statistics.getMean(ryamu));
+    System.out.println(Statistics.toString(Statistics.copyOfRange(ryamu, 1, 3)));
     
   }
 }
--- automathicalc/Statistics.java
+++ automathicalc/Statistics.java
@@ -1,37 +1,39 @@
-/**
- * The class Statistics contains methods for measuring the values in a data set.
+/**The class Statistics contains methods for measuring the values in sets of data.
  * @version 0.001
- * @author Daniel Hassanieh
+ * @author Dani Hassanieh
  */
 package automathicalc;
 
 public class Statistics
 {
 
-  /**
-   * Returns the mean of numbers in an array.
-   * @param nums : array of doubles
-   * @return mean of nums
+  /**Returns the mean of numbers in an array.
+   * @param nums : The array of doubles to find the mean of.
+   * @return the mean of nums.
    */
   public static double getMean(double[] nums)
   {
     double total = 0.0;
-    // adds all nums in array together
+    // adds all nums together
     for(double num : nums) total += num;
-    // divides sum of nums by array length, returns mean
+    // divides the sum by the length of the array and returns the mean
     return total / nums.length;
   }
 
-  /**
-   * Returns the median of numbers in an array.
-   * @param nums : array of doubles
+  // add javadoc
+  public static double getSum(double[] nums)
+  {
+    if(nums.length == 0) return 0; 
+    return nums[0] + getSum(copyOfRange(nums, 1, nums.length));
+  }
+
+  /**Returns the median of numbers in an array.
+   * @param nums : an array of doubles to find the median of.
    * @param isSorted : sorts nums if false.
    * @return the median of nums.
    */
-  public static double getMedian(double[] nums, boolean isSorted)
+  public static double getMedian(double[] nums)
   {
-    // sorts nums from least to greatest if isSorted is false
-    if(!isSorted) nums = sortNums(nums);
     double median;
     // if the length of nums is even, the median is the average of the two middle numbers 
     if(nums.length % 2 == 0) median = (nums[nums.length / 2 - 1] + nums[nums.length / 2]) / 2;
@@ -41,8 +43,7 @@ public class Statistics
     return median;    
   }
 
-  /**
-   * Returns the value that appears the most often in an array of doubles.
+  /**Returns the value that appears the most often in an array of doubles.
    * @param nums : an array of doubles to find the mode of.
    * @return the mode of nums.
    */
@@ -53,8 +54,7 @@ public class Statistics
     return 0;
   } */
   
-  /**
-   * Returns unique numbers in an array.
+  /**Returns unique numbers in an array.
    * @param nums array of numbers
    * @return the unique numbers in the array.
    */
@@ -89,8 +89,7 @@ public class Statistics
     return uniqueNums;
   }
 
-  /**
-   * Returns the largest value in an array of doubles.
+  /**Returns the largest value in an array of doubles.
    * @param nums : The array of doubles to find the largest value in.
    * @return the largest value in the array.
    */
@@ -101,8 +100,7 @@ public class Statistics
     return min;
   }
 
-  /**
-   * Returns the smallest value in an array of doubles.
+  /**Returns the smallest value in an array of doubles.
    * @param nums : The array of doubles to find the smallest value in.
    * @return the smallest value in the array.
    */
@@ -113,103 +111,82 @@ public class Statistics
     return max;
   }
 
-  /**
-   * Returns the index of a num in an array
+  /**Returns an array in ascending order using bubble sort.
    * @param nums : array of doubles
-   * @param key : num to look for
-   * @return index of key in array.
-   * @return -1 if key not fond
+   * @return sorted array
    */
-  public static double linearSearch(double[] nums, double key)
+  public static double[] bubbleSort(double[] nums)
   {
-    for(int index = 0; index < nums.length; index++)
-    {
-      if(nums[index] == key)
-        return index;
-    }
-    return -1;
-  }
-
-  public static int binarySearch(double[] nums, double key)
-  {
-    int lowerBound = 0;
-    int upperBound = nums.length;
-    while(lowerBound <= upperBound)
-    {
-      // midpoint between the lower bound and the upper bound
-      int midpoint = (lowerBound + upperBound) / 2;
-      // if num at the midpoint is the key, returns the index of midpoint
-      if(nums[midpoint] == key)
-        return midpoint;
-      else
-      {
-        // if num at the midpoint is greater than the key, the upper bound becomes the midpoint minus one
-        if(nums[midpoint] > key)
-          upperBound = midpoint - 1;
-        // otherwise, the lower bound becomes the midpoint plus one
-        else
-          lowerBound = midpoint + 1;
-      }
-    }
-    // if the key was not found, returns -1
-    return -1;
-  }
-
-  /**
-   * Returns numbers in an array sorted least to greatest.
-   * @param nums : The array of numbers to be sorted.
-   * @return a sorted array from least to greatest.
-   */
-  public static double[] sortNums(double[] nums)
-  {
-    double[] sortedNums = nums.clone();
-    for(int i = 1; i < sortedNums.length; i++)
+    double[] numsSorted = nums.clone();
+    for(int i = 1; i < numsSorted.length; i++)
     {
       boolean skipSort = true;
-      for(int j = 0; j < sortedNums.length - i; j++)
+      for(int j = 0; j < numsSorted.length - i; j++)
       {
-        double first = sortedNums[j];
-        double second = sortedNums[j + 1];
+        double first = numsSorted[j];
+        double second = numsSorted[j + 1];
         if(first > second)
         {
           skipSort = false;
-          sortedNums[j] = second;
-          sortedNums[j + 1] = first;
+          numsSorted[j] = second;
+          numsSorted[j + 1] = first;
         }
       }
       if(skipSort) break;
     }
-    return sortedNums;
+    return numsSorted;
   }
   
-  /**
-   * Returns the number of times a value is found in array of doubles.
+  /**Returns the number of times a value is found in array of doubles.
    * @param nums : An array of doubles for a value to be counted in.
    * @param find : The value to be counted.
    * @return the number of times that a specific value is counted.
    */
-  public static int getNumFrequency(double[] nums, double find)
+  public static int getFrequency(double[] nums, double find)
   {
     int count = 0;
     for(double num : nums) if(num == find) count++;
     return count;
   }
   
-  /**
-   * Returns the number of times each value is found in an array of doubles with a two-dimensional array.
+  /**Returns the number of times each value is found in an array of doubles with a two-dimensional array.
    * @param nums: An array of doubles for each value to be counted.
    * @return the number of times each value is counted, with the second index in each array being the count of the value in the first index.
    */
-  public static double[][] getNumFrequencies(double[] nums)
+  public static double[][] getFrequencies(double[] nums)
   {
     double[] uniqueNums = getUniqueNums(nums);
     double[][] numFrequencies = new double[uniqueNums.length][2];
     for(int i = 0; i < numFrequencies.length; i++)
     {
       numFrequencies[i][0] = uniqueNums[i];
-      numFrequencies[i][1] = getNumFrequency(nums, uniqueNums[i]);
+      numFrequencies[i][1] = getFrequency(nums, uniqueNums[i]);
     }
     // returns the frequencies of each value in a two-dimensional array.
     return numFrequencies;
   }
+
+  // add javadoc
+  public static double[] copyOfRange(double[] nums, int leftBound, int rightBound)
+  {
+    double[] numsCopy = new double[rightBound - leftBound];
+    for(int index = leftBound; index < rightBound; index++)
+    {
+      numsCopy[index - leftBound] = nums[index];
+    }
+    return numsCopy;
+  } // copyOfRange end
+
+  // add javadoc
+  public static String toString(double[] nums)
+  {
+    String numsStr = "[";
+    for(double num : nums)
+    {
+      numsStr += num + ", ";
+    }
+    numsStr = numsStr.substring(0, numsStr.length() - 2) + "]";
+    return numsStr;
+  }
+  
 }
